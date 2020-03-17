fc79061ee0aed26b92eee83dc8aa4e4426823830
Furkan Karataş <furkan.karatas02@gmail.com>
2020-03-17 15:27:23 +0300

    Add temporary home view & url

--- yasozluk/urls.py
+++ yasozluk/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from contents import views as contents_views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", contents_views.home, name="home"),
 ]
