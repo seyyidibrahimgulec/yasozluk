df0d48099148b68a3d61cc156242f49c3fea221a
Furkan Karataş <furkan.karatas02@gmail.com>
2020-03-18 20:34:54 +0300

    Better naming

--- yasozluk/urls.py
+++ yasozluk/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from contents import views as contents_views
+from contents.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
