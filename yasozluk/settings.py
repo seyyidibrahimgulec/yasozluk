e4411fc1a141ae627213c4727942bf7ca47c63c7
Furkan Karataş <furkan.karatas02@gmail.com>
2020-03-22 19:29:10 +0300

    Add authentication urls

--- yasozluk/settings.py
+++ yasozluk/settings.py
@@ -132,3 +132,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
+
+LOGOUT_REDIRECT_URL = "home"
+LOGIN_REDIRECT_URL = "home"
+LOGIN_URL = "login"
--- yasozluk/urls.py
+++ yasozluk/urls.py
@@ -15,10 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
 
 from contents.views import home
+from users.views import SignupView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", home, name="home"),
+    path("signup/", SignupView.as_view(), name="signup"),
+    path("login/", LoginView.as_view(template_name="login.html"), name="login"),
+    path("logout/", LogoutView.as_view(), name="logout"),
 ]
