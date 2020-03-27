294fa44dd06dbdaf7348eebe6e568133c882af99
Furkan Karataş <furkan.karatas02@gmail.com>
2020-03-28 02:29:48 +0300

    Add upvotes&downvotes properties and implement random entries func
    - Add upvotes & downvotes properties to get number of upvotes &
    downvotes
    
    - Implement eksisozluk's main page entries function. This
    function picks "pick_n" entries from "top_n" most liked entries.
    Entries' like score calculated as upvotes - downvotes

--- contents/models.py
+++ contents/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.utils import timezone
+from django.db.models import Count, Q
+
+from interactions.enums import VoteType
 
 
 class Topic(models.Model):
@@ -15,6 +17,26 @@ class Entry(models.Model):
     created_by = models.ForeignKey(to=User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True, null=True)
+
+    @property
+    def upvotes(self):
+        return self.vote_set.filter(vote=VoteType.upvote).count()
+
+    @property
+    def downvotes(self):
+        return self.vote_set.filter(vote=VoteType.downvote).count()
+
+    def get_random_most_liked_entries(top_n=20, pick_n=5):
+        # TODO: increase top_n after database increased
+        """
+        Returns random "pick_n" entries of most liked "top_n" entries
+        """
+        upvotes = Count("vote", filter=Q(vote__vote=VoteType.upvote))
+        downvotes = Count("vote", filter=Q(vote__vote=VoteType.downvote))
+        score = upvotes - downvotes
+        return sample(
+            list(Entry.objects.annotate(score=score).order_by("-score")[:top_n]), pick_n
+        )
 
 
 class Channel(models.Model):
